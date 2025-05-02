6ad2da02a827c42bc16eac1560892d60b559e182
David Chou <qq3025566@gmail.com>
2025-05-02 09:06:16 +0000

    feat: update settings

--- scripts/apply-site-settings.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Starting site settings configuration..."
-cd /var/www/discourse
-su discourse -c "bundle exec rails r /var/discourse/shared/standalone/scripts/apply-site-settings.rb"
-echo "Settings configuration completed!"
--- scripts/apply-themes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting theme installation process..."
-
-cd /var/www/discourse
-su discourse -c "bundle exec rails runner /shared/scripts/apply-themes.rb"
-
-echo "Theme installation process completed!"
