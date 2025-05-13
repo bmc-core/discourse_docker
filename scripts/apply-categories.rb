7848f09ff44e47ab8fd577472111a7374122978c
krisshih <king150111@gmail.com>
2025-05-13 09:48:28 +0000

    feat: add ruby script to sync categories from yaml config

---
+++ scripts/apply-categories.rb
@@ -0,0 +1,108 @@
+require 'yaml'
+
+file_path = '/shared/categories.yml'
+categories_data = YAML.load_file(file_path)
+
+def apply_category_settings(category, data)
+  # Style Type and Emoji
+  if data["style_type"] && data["emoji"]
+    category.style_type = data["style_type"]
+    category.emoji = data["emoji"]
+    category.save!
+  end
+
+  # Custom Fields
+  if data["custom_fields"]
+    data["custom_fields"].each do |key, value|
+      category.custom_fields[key] = value
+    end
+    category.save_custom_fields
+  end
+
+  # Tag Groups
+  if data["tag_groups"]
+    data["tag_groups"].each do |tg|
+      tag_group = TagGroup.find_by(name: tg)
+      if tag_group && !category.tag_groups.include?(tag_group)
+        category.tag_groups << tag_group
+      end
+    end
+  end
+
+  # Security
+  if data["security"]
+    category.category_groups.destroy_all
+    data["security"].each do |sec|
+      group = Group.find_by(name: sec["group"])
+      if group
+      category.category_groups.create!(group: group, permission_type: sec["permission_type"])
+      end
+    end
+  end
+end
+
+def update_category_attributes(category, data)
+  category.update!(
+    color: data["color"],
+    description: data["description"],
+    position: data["position"],
+    show_subcategory_list: data["show_subcategory_list"],
+    subcategory_list_style: data["subcategory_list_style"],
+    default_list_filter: data["default_list_filter"]
+  )
+end
+
+puts " "
+categories_data["categories"].each do |cat_data|
+  existing_parent = Category.find_by(name: cat_data["name"])
+  if existing_parent
+    puts "========================================================"
+    puts "[Category] already exists, skip create: #{cat_data["name"]}"
+    parent_category = existing_parent
+    update_category_attributes(parent_category, cat_data)
+    apply_category_settings(parent_category, cat_data)
+  else
+    parent_category = Category.create!(
+      name: cat_data["name"],
+      color: cat_data["color"],
+      user_id: eval(cat_data["user_id"]),
+      description: cat_data["description"],
+      position: cat_data["position"],
+      show_subcategory_list: cat_data["show_subcategory_list"],
+      subcategory_list_style: cat_data["subcategory_list_style"],
+      default_list_filter: cat_data["default_list_filter"]
+    )
+    apply_category_settings(parent_category, cat_data)
+    puts "========================================================"
+    puts "[Category] create successful: #{cat_data["name"]}"
+  end
+
+  # Create Subcategories
+  if cat_data["subcategories"]
+    cat_data["subcategories"].each do |sub_data|
+      #existing_sub = Category.find_by(name: sub_data["name"])
+      existing_sub = Category.find_by(name: sub_data["name"], parent_category_id: parent_category.id)
+      if existing_sub
+        puts "[Subcategory] already exists, skip create: #{sub_data["name"]}"
+        subcategory = existing_sub
+        update_category_attributes(subcategory, sub_data)
+        apply_category_settings(subcategory, sub_data)
+      else
+        puts "Creating subcategory: #{sub_data["name"]}"
+        subcategory = Category.create!(
+          name: sub_data["name"],
+          color: sub_data["color"],
+          user_id: eval(sub_data["user_id"]),
+          position: sub_data["position"],
+          parent_category_id: parent_category.id,
+          default_list_filter: sub_data["default_list_filter"]
+        )
+      end
+      apply_category_settings(subcategory, sub_data)
+    end
+  end
+end
+Category.find_by(name: "Staff").update!(position: 58)
+Category.find_by(name: "General").update!(position: 59)
+Category.find_by(name: "Site Feedback").update!(position: 60)
+puts "\n============= Apply categories successful ============="
